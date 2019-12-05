8c26aef944fc47ae35e7c1a89a15ee74e275dc70
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-12-04 20:29:43 -0500

    working on the stretch problem, changing the app name into server

--- index.js
+++ index.js
@@ -1,72 +1,114 @@
 const express = require('express')
-let db = require('./data/db')
+let users = require('./data/db.js')
 
-// this is done to create an instance of our express app
 const app = express()
-//this is one case of middleware "use" 
+ 
 app.use(express.json())
 
-//defining route
-app.get('/', (req, res) => {
-    res.json({ message: "Welcome to our API!"})
-})
-
-app.get('/users', (req, res) => {
-    // this is calling the db we imported to display all users
-    res.json(db) 
-})
-
-app.get('/users/:id', (req, res) => {
-    const user = db.find(row => row.id === req.params.id)
-    if (user) {
-        res.json(user)
-    } else {
-        res.status(404).json({error: "No User Found"})
-    }
-})
-
-app.delete('/users/:id', (req, res) => {
-    const user = db.find(row => row.id === req.params.id)
-
-    if (user) {
-        db = db.filter(row => row.id !== req.params.id)
-        res.json(user)
-    } else {
-        res.status(404).json({ error: "User not found"})
-    }
-})
-
-app.post('/users', (req, res) => {
-    const user = req.body
-    if (!req.body.name,) {
-            return res.status(400).json({ error: "Need requied field"})
-        }
-
-    const newUser = {
-        id: String(db.length + 1),
-        name: req.body.name,
-        bio: req.body.bio,
-    }
-    db.push(newUser)
-    res.status(201).json(newUser)
-})
-
-app.put('/users/:id', (req, res) => {
-    const user = db.find(row => row.id === req.params.id)
-    if (!req.body.name,
-        !req.body.bio,
-        ) {
-            return res.status(400).json({ error: "Please fill in the updated info"})
-        }
-    const editUser = {
-        name: req.body.name,
-        bio: req.body.bio
-
-    }
-})
-
-const port = 8080
-const host = '127.0.0.1'
-app.listen(port, host, () => {
-    console.log(`Server running at http://${host}:${port}`)
-})
+app.get("/", (req, res) => {
+	res.send("Hey man");
+  });
+  
+  app.post("/api/users", (req, res) => {
+	const { name, bio } = req.body;
+  
+	if (!name || !bio) {
+	  res
+		.status(400)
+		.json({ error: "Please enter a name and a bio for the user" });
+	} else {
+	  users
+		.insert(req.body)
+		.then(user => {
+		  res.status(201).json(user);
+		})
+		.catch(error => {
+		  res
+			.status(500)
+			.json({ error: "There was an error processing your request" });
+		});
+	}
+  });
+  
+  app.get("/api/users", (req, res) => {
+	users
+	  .find()
+	  .then(users => {
+		res.status(200).json(users);
+	  })
+	  .catch(err => {
+		res.status(500).json({
+		  err,
+		  err: "the users information could not be retrieved"
+		});
+	  });
+  });
+  
+  app.get("/api/users/:id", (req, res) => {
+	const { id } = req.params;
+	users
+	  .findById(id)
+	  .then(user => {
+		if (user) {
+		  res.status(200).json(user);
+		} else {
+		  res
+			.status(404)
+			.json({ message: "The user with the specified ID does not exist" });
+		}
+	  })
+	  .catch(error => {
+		res
+		  .status(500)
+		  .json({ error, error: "The user information could not be retrieved." });
+	  });
+  });
+  
+  app.delete("/api/users/:id", (req, res) => {
+	const { id } = req.params;
+  
+	users
+	  .remove(id)
+	  .then(count => {
+		if (count && count > 0) {
+		  res.status(200).json({
+			message: "The user was deleted"
+		  });
+		} else {
+		  res.status(404).json({
+			message: "The user with the specified ID does not exist."
+		  });
+		}
+	  })
+	  .catch(error => {
+		res.status(500).json({ error, error: "The user could not be removed" });
+	  });
+  });
+  
+  app.put("/api/users/:id", (req, res) => {
+	const { name, bio } = req.body;
+	const { id } = req.params;
+  
+	if (!name || !bio) {
+	  res.status(400).json({ error: "please provide name and bio for the user" });
+	} else {
+	  users
+		.update(id, req.body)
+		.then(user => {
+		  if (user) {
+			res.status(200).json(req.body);
+		  } else {
+			res.status(404).json({
+			  message: "the user with the specified ID does not exist"
+			});
+		  }
+		})
+		.catch(error => {
+		  res.status(500).json({ error, error: "The user could not be updated" });
+		});
+	}
+  });
+  
+  app.listen(8080, () =>
+	console.log("Welcome you are in a safe place")
+  );
